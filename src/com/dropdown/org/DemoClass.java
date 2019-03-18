5b1104a8426322c814092e2a8b8e9d51322fe0f2
Eswar <peswar1@ford.com>
2019-03-18 11:29:27 +0530

    Committing first program in git

---
+++ src/com/dropdown/org/DemoClass.java
@@ -0,0 +1,13 @@
+package com.dropdown.org;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b= 20;
+		int c= a+b;
+		System.out.println("Addition of two numbers :"+c);
+
+	}
+
+}
