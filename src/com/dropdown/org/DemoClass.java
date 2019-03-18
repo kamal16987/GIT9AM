d06250b4317e94de33fc3d5d8d675f3f80da82cb
Dhanalakshmi <dhanalakshmi7@gmail.com>
2019-03-18 11:31:52 +0530

    Committing second time

--- src/com/dropdown/org/DemoClass.java
+++ src/com/dropdown/org/DemoClass.java
@@ -5,8 +5,7 @@ public class DemoClass {
 	public static void main(String[] args) {
 		int a=10;
 		int b= 20;
-		int c= a+b;
-		System.out.println("Addition of two numbers :"+c);
+		System.out.println("Addition of two numbers :"+a+b);
 
 	}
 
